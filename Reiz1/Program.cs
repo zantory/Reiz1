dc6f699684c9c0ca1e425eef1c94de58619cd730
vassu <vassu@TRENZER899>
2023-02-09 16:41:23 +0200

    Add project files.

---
+++ Reiz1/Program.cs
@@ -0,0 +1,48 @@
+﻿ using System;
+
+
+
+namespace ConsoleApp
+
+{
+
+    class FindAngle
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            int hr, min;
+
+            Console.WriteLine("Enter Hour: ");
+
+            hr = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Minute: ");
+
+            min = Convert.ToInt32(Console.ReadLine());
+
+
+
+            double hrDeg = (hr * 30) + (min * 30 / 60);
+
+            double minDeg = min * 6;
+
+
+
+            double dif = Math.Abs(hrDeg - minDeg);
+
+            if (dif > 180)
+                dif = 360 - dif;
+
+            Console.WriteLine("Angle b/w both minute and hour hand is : {0} ", dif);
+
+            Console.ReadLine();
+
+        }
+
+    }
+
+}
